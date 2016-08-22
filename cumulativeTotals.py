eb3f8c7a4ee80fc3a7264f68b8024f2b01adbe27
jc374321 <matthew.wallwork1@my.jcu.edu.au>
2016-08-22 12:12:26 +1000

    change income input to a str.format and made the Income Report into a function called printReport

--- cumulativeTotals.py
+++ cumulativeTotals.py
@@ -6,16 +6,18 @@ Starter code for cumulative total income program
 
 def main():
     incomes = []
-    months = int(input("How many months? "))
+    numberOfMonths = int(input("How many months? "))
 
-    for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+    for month in range(1, numberOfMonths + 1):
+        income = float(input("Enter income for month {}: ".format(month)))
         incomes.append(income)
+    printReport(numberOfMonths, incomes)
 
+def printReport(monthCount, incomeList):
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, months + 1):
-        income = incomes[month - 1]
+    for month in range(1, monthCount + 1):
+        income = incomeList[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
 
